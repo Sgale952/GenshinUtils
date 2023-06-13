36df2c104d247508c3af0a96484fdd251be01e88
Sgale <bread952@list.ru>
2023-06-13 07:25:07 +0700

    Add main buttons function

--- src/main/java/sgale952/gutils/MainController.java
+++ src/main/java/sgale952/gutils/MainController.java
@@ -11,13 +11,43 @@ import javafx.stage.Stage;
 public class MainController {
 
     @FXML
-    private Button aboutButton, calculatorButton, generatorButton;
+    private Button aboutButton, mainButton, calculatorButton, generatorButton, menuCalcButton, menuGenButton;
 
     //not work
     public void aboutButtonHandler() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("about.fxml"));
         Stage window = (Stage) aboutButton.getScene().getWindow();
         window.setScene(new Scene(fxmlLoader.load(), 670, 405));
+    }
+
+    public void mainButtonHandler() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Stage window = (Stage) mainButton.getScene().getWindow();
+        window.setScene(new Scene(fxmlLoader.load(), 670, 405));
+    }
+
+    public void calculatorButtonHandler() throws IOException {
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("Calculator/calculator.fxml"));
+        Stage window = (Stage) calculatorButton.getScene().getWindow();
+        window.setScene(new Scene(fxmlloader.load(), 670, 405));
+    }
+
+    public void generatorButtonHandler() throws IOException {
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("Generator/generator.fxml"));
+        Stage window = (Stage) generatorButton.getScene().getWindow();
+        window.setScene(new Scene(fxmlloader.load(), 670, 405));
+    }
+
+    public void menuCalcButtonHandler() throws IOException {
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Stage window = (Stage) menuCalcButton.getScene().getWindow();
+        window.setScene(new Scene(fxmlloader.load(), 670, 405));
+    }
+
+    public void menuGenButtonHandler() throws IOException {
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Stage window = (Stage) menuGenButton.getScene().getWindow();
+        window.setScene(new Scene(fxmlloader.load(), 670, 405));
     }
 
 }
